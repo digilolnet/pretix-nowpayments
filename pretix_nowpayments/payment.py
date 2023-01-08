b9af3a83fcc7aa386c26323c3f25d5db89b9fac4
Siren <irem@digilol.net>
2023-01-08 02:10:53 +0100

    Check NOWPayments API status, available currencies and execute template

--- pretix_nowpayments/payment.py
+++ pretix_nowpayments/payment.py
@@ -1,6 +1,12 @@
+import logging
 from collections import OrderedDict
 
+from nowpayments import NOWPayments
+from nowpayments.sandbox import NOWPaymentsSandbox
+
 from django import forms
+from django.contrib import messages
+from django.template.loader import get_template
 
 from pretix.base.decimal import round_decimal
 from pretix.base.models import Event, Order, OrderPayment, OrderRefund, Quota
@@ -8,6 +14,8 @@ from pretix.base.payment import BasePaymentProvider, PaymentException
 from pretix.base.services.mail import SendMailException
 from pretix.base.settings import SettingsSandbox
 from pretix.multidomain.urlreverse import build_absolute_uri
+
+logger = logging.getLogger('pretix.plugins.nowpayments')
 
 SUPPORTED_CURRENCIES = ['EUR']
 LOCAL_ONLY_CURRENCIES = ['EUR']
@@ -33,3 +41,49 @@ class NowPayments(BasePaymentProvider):
                 ('api_key', forms.CharField(label='API key'))
             ])
         return d
+
+    def init_api(self):
+        if self.settings.get('endpoint') == 'sandbox':
+            payment = NOWPaymentsSandbox(self.settings.get('api_key'))
+            # This is because the nowpayments library is broken, will
+            # remove these lines once my PR is merged to fix it.
+            payment._API_URL="https://api-sandbox.nowpayments.io/v1/{}"
+            payment._IS_SANDBOX=True
+            return payment
+        else:
+            payment = NOWPayments(self.settings.get('api_key'))
+            return payment
+
+    def checkout_prepare(self, request, cart):
+        nowp = self.init_api()
+        try:
+            api_status = nowp.get_api_status()
+            if api_status['message'] != 'OK':
+                messages.error("NOWPayments API is currently unavailable. Try again later.")
+                return False
+        except Exception as e:
+            messages.error(request,
+                '{}: {}'.format("We had trouble communicating with NOWPayments.", e))
+            return False
+
+        try:
+            currencies = nowp.get_available_currencies()
+            if 'xmr' not in currencies['currencies']:
+                messages.error("Monero is currently unavailable for payments.")
+                return False
+        except Exception as e:
+            messages.error(request,
+                '{}: {}'.format("We had trouble communicating with NOWPayments.", e))
+            return False
+
+        return True
+
+    def payment_is_valid_session(self, request):
+        return True
+
+    def checkout_confirm_render(self, request) -> str:
+        # Displayed when the user selected this provider on the 'confirm order'
+        # page.
+        template = get_template('pretix_nowpayments/checkout_payment_confirm.html')
+        ctx = {'request': request, 'event': self.event, 'settings': self.settings}
+        return template.render(ctx)
